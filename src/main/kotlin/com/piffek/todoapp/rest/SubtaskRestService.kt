bed65096451db507fa5600b354b04ba0c43fac44
Patryk Piwko <patryk.piwko@god-se.com>
2023-01-24 22:13:11 +0100

    refactor rest services

--- src/main/kotlin/com/piffek/todoapp/rest/SubtaskRestService.kt
+++ src/main/kotlin/com/piffek/todoapp/rest/SubtaskRestService.kt
@@ -5,11 +5,13 @@ import com.piffek.todoapp.subtask.domain.SubtaskDto
 import java.util.UUID
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class SubtaskController(private val subtaskFacade: SubtaskFacade) {
-    @GetMapping("/subtasks/{taskId}")
+@RequestMapping("/rest-api/subtask")
+class SubtaskRestService(private val subtaskFacade: SubtaskFacade) {
+    @GetMapping("/all/{taskId}")
     fun showAllSubtaskAssignedToTask(@PathVariable("taskId") taskId: UUID): List<SubtaskDto> =
         SubtaskDto.from(subtaskFacade.findSubtasksForTask(taskId))
 }
--- src/main/kotlin/com/piffek/todoapp/rest/TaskRestService.kt
+++ src/main/kotlin/com/piffek/todoapp/rest/TaskRestService.kt
@@ -3,13 +3,14 @@ package com.piffek.todoapp.rest
 import com.piffek.todoapp.task.TaskFacade
 import com.piffek.todoapp.task.domain.TaskDto
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@RestController("/task")
-class TaskController(private val taskFacade: TaskFacade) {
-
+@RestController
+@RequestMapping("/rest-api/task")
+class TaskRestService(private val taskFacade: TaskFacade) {
     @GetMapping
     fun showAll(): List<TaskDto> = TaskDto.from(taskFacade.findAll())
 
