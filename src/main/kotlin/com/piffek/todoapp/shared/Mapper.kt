33a42a410141614b533fd11d357bdb826d33c1c9
Patryk Piwko <patryk.piwko@god-se.com>
2023-01-24 14:38:04 +0100

    simple refactor

--- src/main/kotlin/com/piffek/todoapp/shared/Mapper.kt
+++ src/main/kotlin/com/piffek/todoapp/shared/Mapper.kt
@@ -2,5 +2,5 @@ package com.piffek.todoapp.shared
 
 interface Mapper<IN, OUT> {
     fun from(input: IN): OUT
-    fun from(inputMap: MutableList<IN>): List<OUT>
+    fun from(inputList: List<IN>): List<OUT>
 }
